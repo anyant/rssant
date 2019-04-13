5d5446ffa8e6135b3058319f4f9660ae3698b816
guyskk <guyskk@qq.com>
2019-04-13 16:32:18 +0800

    update scripts

--- scripts/postgres_start.sh
+++ scripts/postgres_start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-docker run --name rssant_postgres --rm -ti \
--p 127.0.0.1:5432:5432 \
--e POSTGRES_USER=rssant \
--e POSTGRES_PASSWORD=rssant \
--v $(pwd)/data/postgres:/var/lib/postgresql/data \
-postgres:10
+docker network create rssant || true
+docker volume create rssant_postgres || true
+
+docker run -d \
+    --name rssant_postgres \
+    --network rssant \
+    -p 127.0.0.1:5432:5432 \
+    -e "POSTGRES_USER=rssant" \
+    -e "POSTGRES_PASSWORD=rssant" \
+    -e "POSTGRES_DB=rssant" \
+    -v rssant_postgres:/var/lib/postgresql/data \
+    postgres:10
--- scripts/redis_start.sh
+++ scripts/redis_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run --name rssant_redis --rm -ti \
--p 127.0.0.1:6379:6379 \
--v $(pwd)/data/redis:/data \
-redis:4
+docker network create rssant || true
+
+docker run -d \
+    --name rssant_redis \
+    --network rssant \
+    -p 127.0.0.1:6379:6379 \
+    redis:4
