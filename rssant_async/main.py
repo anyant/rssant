12d9ebee44325d11bb25685205e2d19bf74e97b6
guyskk <guyskk@qq.com>
2019-06-09 20:13:16 +0800

    integrate sentry to async api

--- rssant_async/main.py
+++ rssant_async/main.py
@@ -4,16 +4,26 @@ import logging
 import django
 from aiohttp import web
 from aiojobs.aiohttp import setup as setup_aiojobs
+import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
+from rssant.settings import ENV_CONFIG
 from .views import routes
 from .callback_client import CallbackClient
 from .redis_dao import REDIS_DAO
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rssant.settings')
 django.setup()
 
 LOG_FORMAT = "%(levelname)1.1s %(asctime)s %(name)s:%(lineno)-4d %(message)s"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+if ENV_CONFIG.sentry_enable:
+    sentry_sdk.init(
+        dsn=ENV_CONFIG.sentry_dsn,
+        integrations=[AioHttpIntegration()]
+    )
 
 api = web.Application()
 api.router.add_routes(routes)
