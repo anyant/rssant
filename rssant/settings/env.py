c5eb906f3404f903b19c1b950f503f1d79a14db3
guyskk <guyskk@qq.com>
2019-07-02 21:53:34 +0800

    fix django_db_init

--- rssant/settings/env.py
+++ rssant/settings/env.py
@@ -7,20 +7,20 @@ from validr import T, modelclass, fields, Invalid
 @modelclass
 class EnvConfig:
     debug = T.bool.default(True).desc('debug')
-    root_url = T.url.default('http://127.0.0.1:6789')
-    async_url_prefix = T.url.default('http://127.0.0.1:6786/api/v1')
-    async_callback_url_prefix = T.url.default('http://127.0.0.1:6788/api/v1')
+    root_url = T.url.default('http://localhost:6789')
+    async_url_prefix = T.url.default('http://localhost:6786/api/v1')
+    async_callback_url_prefix = T.url.default('http://localhost:6788/api/v1')
     secret_key = T.str.default('8k1v_4#kv4+3qu1=ulp+@@#65&++!fl1(e*7)ew&nv!)cq%e2y')
     allow_private_address = T.bool.default(False)
     check_feed_minutes = T.int.min(1).default(30)
     # postgres database
-    pg_host = T.str.default('127.0.0.1').desc('postgres host')
+    pg_host = T.str.default('localhost').desc('postgres host')
     pg_port = T.int.default(5432).desc('postgres port')
     pg_db = T.str.default('rssant').desc('postgres database')
     pg_user = T.str.default('rssant').desc('postgres user')
     pg_password = T.str.default('rssant').desc('postgres password')
     # celery redis
-    redis_url = T.str.default('redis://127.0.0.1:6379/0').desc('redis url')
+    redis_url = T.str.default('redis://localhost:6379/0').desc('redis url')
     # github login
     github_client_id = T.str.optional
     github_secret = T.str.optional
@@ -30,6 +30,7 @@ class EnvConfig:
     # celery sentry
     is_celery_process = T.bool.optional
     # email smtp
+    admin_email = T.email.default('admin@localhost.com')
     smtp_enable = T.bool.default(False)
     smtp_host = T.str.optional
     smtp_port = T.int.min(0).optional
--- scripts/django_db_init.py
+++ scripts/django_db_init.py
@@ -1,3 +1,6 @@
+import logging
+from urllib.parse import urlparse
+
 import django.db
 from django.db import connection
 from django.conf import settings
@@ -7,7 +10,7 @@ from allauth.socialaccount.models import SocialApp
 from allauth.socialaccount.providers.github.provider import GitHubProvider
 
 
-User = get_user_model()
+LOG = logging.getLogger(__name__)
 
 
 def run():
@@ -15,21 +18,47 @@ def run():
     with connection.cursor() as cursor:
         try:
             cursor.execute('create extension hstore;')
-        except django.db.ProgrammingError:
-            pass  # ignore: already exists
+        except django.db.ProgrammingError as ex:
+            if 'already exists' not in str(ex):
+                raise
+            LOG.info(str(ex).strip())
+        else:
+            LOG.info('extension hstore created')
     # create superuser
-    User.objects.create_superuser('admin', 'admin@anyant.com', 'admin')
+    User = get_user_model()
+    admin_email = settings.ENV_CONFIG.admin_email
+    try:
+        admin = User.objects.get(username='admin')
+    except User.DoesNotExist:
+        LOG.info('create admin user')
+        User.objects.create_superuser('admin', admin_email, 'admin')
+    else:
+        LOG.info('update admin user email')
+        admin.email = admin_email
+        admin.save()
     # update site
+    root_url = urlparse(settings.ENV_CONFIG.root_url)
+    LOG.info('update site info')
     site = Site.objects.get_current()
-    site.domain = '127.0.0.1:6789'
-    site.name = '127.0.0.1:6789'
+    site.domain = root_url.netloc
+    site.name = root_url.netloc
     site.save()
     # create social app
     client_id = settings.SOCIAL_APP_GITHUB['client_id']
     secret = settings.SOCIAL_APP_GITHUB['secret']
-    social_app = SocialApp(
-        provider=GitHubProvider.id, name='github',
-        client_id=client_id, secret=secret)
+    provider = GitHubProvider.id
+    try:
+        social_app = SocialApp.objects.get(provider=provider, name='github')
+    except SocialApp.DoesNotExist:
+        LOG.info('create github social app')
+        social_app = SocialApp(
+            provider=provider, name='github',
+            client_id=client_id, secret=secret)
+    else:
+        LOG.info('update github social app')
+        social_app.client_id = client_id
+        social_app.secret = secret
     social_app.save()
-    social_app.sites.add(site)
-    social_app.save()
+    if not social_app.sites.filter(pk=site.pk).exists():
+        social_app.sites.add(site)
+        social_app.save()
