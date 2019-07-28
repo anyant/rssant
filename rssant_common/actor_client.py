0162cc4fa2aaf83ce06a03295d3ce1abd5208a7a
guyskk <guyskk@qq.com>
2019-07-28 18:36:59 +0800

    update actor client config

--- rssant_common/actor_client.py
+++ rssant_common/actor_client.py
@@ -1,19 +1,11 @@
 from actorlib import ActorClient, ActorMessage, ActorRegistery
-
-
-registery_node_spec = {
-    'name': 'scheduler',
-    'modules': ['scheduler'],
-    'networks': [{
-        'name': 'localhost',
-        'url': 'http://127.0.0.1:6790/api/v1/scheduler',
-    }]
-}
+from rssant.settings import ENV_CONFIG
 
 
 class SchedulerActorClient:
     def __init__(self, registery_node_spec):
-        self.registery = ActorRegistery(registery_node_spec=registery_node_spec)
+        self.registery = ActorRegistery(
+            registery_node_spec=registery_node_spec)
         self.client = ActorClient(registery=self.registery)
 
     def tell(self, dst, content):
@@ -41,4 +33,4 @@ class SchedulerActorClient:
         return self.client.ask('scheduler.proxy_ask', dict(dst=dst, content=content))
 
 
-scheduler = SchedulerActorClient(registery_node_spec)
+scheduler = SchedulerActorClient(ENV_CONFIG.registery_node_spec)
