ef997473505dd6dc06e5336db71934e1080a8df7
guyskk <guyskk@qq.com>
2019-10-27 22:21:24 +0800

    fix update_feed_monthly_story_count transaction

--- rssant_api/management/commands/rss.py
+++ rssant_api/management/commands/rss.py
@@ -74,10 +74,10 @@ def update_feed_story_publish_period(feeds=None):
 @main.command()
 @click.option('--feeds', help="feed ids, separate by ','")
 def update_feed_monthly_story_count(feeds=None):
-    with transaction.atomic():
-        feed_ids = _get_feed_ids(feeds)
-        LOG.info('total %s feeds', len(feed_ids))
-        for feed_id in tqdm.tqdm(feed_ids, ncols=80, ascii=True):
+    feed_ids = _get_feed_ids(feeds)
+    LOG.info('total %s feeds', len(feed_ids))
+    for feed_id in tqdm.tqdm(feed_ids, ncols=80, ascii=True):
+        with transaction.atomic():
             Story.refresh_feed_monthly_story_count(feed_id)
 
 
