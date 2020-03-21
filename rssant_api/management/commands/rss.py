e134154f23c39b34825f339b9ebbd408bb2476ff
guyskk <guyskk@qq.com>
2020-03-21 15:27:12 +0800

    add script fix_user_story_offset

--- rssant_api/management/commands/rss.py
+++ rssant_api/management/commands/rss.py
@@ -249,3 +249,27 @@ def delete_invalid_feeds(days=1, limit=100, threshold=99):
             UnionFeed.bulk_delete(delete_feed_ids)
             click.echo('Done!')
     return error_feeds
+
+
+@main.command()
+def fix_user_story_offset():
+    sql = """
+    SELECT us.id, us."offset", story."offset"
+    FROM rssant_api_userstory AS us
+    LEFT OUTER JOIN rssant_api_story AS story
+        ON us.story_id=story.id
+    WHERE us."offset" != story."offset"
+    """
+    items = []
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        for us_id, us_offset, story_offset in cursor.fetchall():
+            items.append((us_id, us_offset, story_offset))
+    click.echo(f'total {len(items)} mismatch user story offset')
+    if not items:
+        return
+    with transaction.atomic():
+        for us_id, us_offset, story_offset in tqdm.tqdm(items, ncols=80, ascii=True):
+            UserStory.objects.filter(pk=us_id).update(offset=-us_offset)
+        for us_id, us_offset, story_offset in tqdm.tqdm(items, ncols=80, ascii=True):
+            UserStory.objects.filter(pk=us_id).update(offset=story_offset)
