75184371d41e7c9e25d8ba87471eeda35212bc2a
guyskk <guyskk@qq.com>
2020-03-22 01:22:39 +0800

    add last-modified header to changelog response

--- rssant/views.py
+++ rssant/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.utils import timezone
+from django.views.decorators.http import condition
 
 from rssant_config import CONFIG
 from rssant_common.changelog import ChangeLogList
@@ -22,16 +24,18 @@ CHANGE_LOGS = ChangeLogList(
     title='蚁阅更新日志',
     link=CONFIG.root_url,
 )
-
+CHANGE_LOGS_LAST_MODIFIED = timezone.now()
 CHANGE_LOGS_ATOM = CHANGE_LOGS.to_atom()
 CHANGE_LOGS_HTML = CHANGE_LOGS.to_html()
 
 
+@condition(last_modified_func=lambda r: CHANGE_LOGS_LAST_MODIFIED)
 def changelog_atom(request):
     return HttpResponse(
         CHANGE_LOGS_ATOM, content_type='text/xml', charset='utf-8')
 
 
+@condition(last_modified_func=lambda r: CHANGE_LOGS_LAST_MODIFIED)
 def changelog_html(request):
     return HttpResponse(
         CHANGE_LOGS_HTML, content_type='text/html', charset='utf-8')
