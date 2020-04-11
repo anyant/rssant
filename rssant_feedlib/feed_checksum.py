d279501d36f1daa504c86ae6d666a9efb3569ef5
guyskk <guyskk@qq.com>
2020-04-11 14:00:44 +0800

    test and fix feed checksum

--- rssant_feedlib/feed_checksum.py
+++ rssant_feedlib/feed_checksum.py
@@ -3,20 +3,25 @@ from collections import OrderedDict
 import itertools
 import struct
 import hashlib
-import brotli
 
 
 class FeedChecksum:
     """
-    At most: 8 * 2 * 500 = 8KB, about 3KB after brotli compress
+    At most: (4 + 8) * 300 = 3.6KB, can not compress
     +---------+------------------+--------------------+
-    | 1 byte  |             8 * 2 * N bytes           |
+    | 1 byte  |             (4 + 8) * N bytes         |
     +---------+------------------+--------------------+
     | version | story_ident_hash | story_content_hash |
     +---------+------------------+--------------------+
     """
 
+    _key_len = 4
+    _val_len = 8
+    _key_val_len = _key_len + _val_len
+
     def __init__(self, items: List[Tuple[bytes, bytes]] = None, version: int = 1):
+        if version != 1:
+            raise ValueError(f'not support version {version}')
         self.version = version
         self._map = OrderedDict()
         for key, value in items or []:
@@ -24,65 +29,74 @@ class FeedChecksum:
             self._map[key] = value
 
     def __repr__(self):
-        return '<{} version={} items={}>'.format(
-            type(self).__name__, self.version, len(self._map))
+        return '<{} version={} size={}>'.format(
+            type(self).__name__, self.version, self.size())
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return (self.version == other.version and self._map == other._map)
+
+    def size(self) -> int:
+        return len(self._map)
 
     def copy(self) -> "FeedChecksum":
         items = list(self._map.items())
         return FeedChecksum(items, version=self.version)
 
-    def _hash(self, value: str) -> bytes:
-        """8 bytes md5"""
-        return hashlib.md5(value.encode('utf-8')).digest()[:8]
+    def _hash(self, value: str, length: int) -> bytes:
+        return hashlib.md5(value.encode('utf-8')).digest()[:length]
 
     def update(self, ident: str, content: str) -> bool:
         """
         由于哈希碰撞，可能会出现:
-            1. 有更新但内容哈希值没变，导致误判为无更新
-            2. 多个ID哈希值一样，导致误判为有更新
+            1. 有更新但内容哈希值没变，导致误判为无更新。不能接受。
+            2. 多个ID哈希值一样，导致误判为有更新。可以接受。
         """
-        key = self._hash(ident)
+        if not ident or not content:
+            raise ValueError('ident and content can not empty')
+        key = self._hash(ident, self._key_len)
         old_sum = self._map.get(key)
-        new_sum = self._hash(content)
+        new_sum = self._hash(content, self._val_len)
         if (not old_sum) or old_sum != new_sum:
             self._map[key] = new_sum
             return True
         return False
 
     def _check_key_value(self, key: bytes, value: bytes):
-        assert len(key) == 8, 'key length must be 8 bytes'
-        assert len(value) == 8, 'value length must be 8 bytes'
+        if len(key) != self._key_len:
+            raise ValueError(f'key length must be {self._key_len} bytes')
+        if len(value) != self._val_len:
+            raise ValueError(f'value length must be {self._val_len} bytes')
 
     def dump(self, limit=None) -> bytes:
         length = len(self._map)
         buffer_n = length if limit is None else min(length, limit)
-        buffer = bytearray(1 + 16 * buffer_n)
+        buffer = bytearray(1 + self._key_val_len * buffer_n)
         struct.pack_into('>B', buffer, 0, self.version)
-        offset = 1
         items = self._map.items()
         if limit is not None and length > limit:
-            items = itertools.islice(items, length - limit)
+            items = itertools.islice(items, length - limit, length)
+        offset = 1
         for key, value in items:
             self._check_key_value(key, value)
-            buffer[offset: offset + 8] = key
-            buffer[offset + 8: offset + 16] = value
-            offset += 16
-        buffer = brotli.compress(buffer)
-        return buffer
+            buffer[offset: offset + self._key_len] = key
+            buffer[offset + self._key_len: offset + self._key_val_len] = value
+            offset += self._key_val_len
+        return bytes(buffer)
 
     @classmethod
     def load(cls, data: bytes) -> "FeedChecksum":
-        data = brotli.decompress(data)
-        version = struct.unpack('>B', data[:1])
+        version = struct.unpack('>B', data[:1])[0]
         if version != 1:
             raise ValueError(f'not support version {version}')
-        n, remain = divmod(len(data) - 1, 16)
+        n, remain = divmod(len(data) - 1, cls._key_val_len)
         if remain != 0:
             raise ValueError(f'unexpect data length {len(data)}')
         items = []
         for i in range(n):
-            offset = 1 + i * 8
-            key = data[offset: offset + 8]
-            value = data[offset + 8: offset + 16]
+            offset = 1 + i * cls._key_val_len
+            key = data[offset: offset + cls._key_len]
+            value = data[offset + cls._key_len: offset + cls._key_val_len]
             items.append((key, value))
         return cls(items, version=version)
---
+++ tests/feedlib/test_feed_checksum.py
@@ -0,0 +1,122 @@
+import random
+
+import pytest
+
+from rssant_feedlib.feed_checksum import FeedChecksum
+
+
+def _random_unicode(length: int) -> str:
+
+    # Update this to include code point ranges to be sampled
+    include_ranges = [
+        (0x0021, 0x0021),
+        (0x0023, 0x0026),
+        (0x0028, 0x007E),
+        (0x00A1, 0x00AC),
+        (0x00AE, 0x00FF),
+        (0x0100, 0x017F),
+        (0x0180, 0x024F),
+        (0x2C60, 0x2C7F),
+        (0x16A0, 0x16F0),
+        (0x0370, 0x0377),
+        (0x037A, 0x037E),
+        (0x0384, 0x038A),
+        (0x038C, 0x038C),
+    ]
+
+    alphabet = [
+        chr(code_point) for current_range in include_ranges
+        for code_point in range(current_range[0], current_range[1] + 1)
+    ]
+    return ''.join(random.choice(alphabet) for i in range(length))
+
+
+def _random_storys(n):
+    storys = []
+    for i in range(n):
+        ident = str(i) * 20
+        content = _random_unicode(random.randint(1, 1000))
+        storys.append((ident, content))
+    return storys
+
+
+def test_feed_checksum_basic():
+    storys = _random_storys(100)
+    checksum = FeedChecksum()
+    for ident, content in storys:
+        assert checksum.update(ident, content) is True
+    assert checksum.size() == 100
+    assert repr(checksum)
+    checksum_bak = checksum.copy()
+    assert checksum_bak == checksum
+    for ident, content in storys:
+        assert checksum.update(ident, content) is False
+    assert checksum_bak == checksum
+
+
+def test_feed_checksum_dump_load():
+    storys = _random_storys(1000)
+    checksum = FeedChecksum()
+    for ident, content in storys:
+        assert checksum.update(ident, content) is True
+    assert checksum.size() == 1000
+    assert repr(checksum)
+    for ident, content in storys:
+        assert checksum.update(ident, content) is False
+
+    data = checksum.dump()
+    loaded = FeedChecksum.load(data)
+    assert loaded.size() == 1000
+    assert loaded == checksum
+
+    data = checksum.dump(limit=500)
+    loaded = FeedChecksum.load(data)
+    assert loaded.size() == 500
+    assert loaded != checksum
+
+    # verify only dump last N items
+    checksum = loaded
+    for ident, content in storys[500:]:
+        assert checksum.update(ident, content) is False
+    assert checksum.size() == 500
+    for ident, content in storys[:500]:
+        assert checksum.update(ident, content) is True
+    assert checksum.size() == 1000
+
+
+def test_feed_checksum_error():
+    # format: 4 bytes + 8 bytes
+    items = [(b'1234', b'12345678')]
+    checksum = FeedChecksum(items)
+    assert checksum.size() == 1
+
+    data = checksum.dump()
+    data = data + b'123'
+    with pytest.raises(ValueError):
+        FeedChecksum.load(data)
+
+    items = [(b'123', b'123456')]
+    with pytest.raises(ValueError):
+        FeedChecksum(items)
+
+    items = [(b'12', b'12345')]
+    with pytest.raises(ValueError):
+        FeedChecksum(items)
+
+
+def benchmark_feed_checksum():
+    for n in range(100, 1001, 100):
+        storys = _random_storys(n)
+        checksum = FeedChecksum()
+        for ident, content in storys:
+            if not checksum.update(ident, content):
+                print(f'n={n} content conflict')
+        for ident, content in storys:
+            if checksum.update(ident, content):
+                print(f'n={n} ident conflict')
+        data = checksum.dump()
+        print(f'{n} items, {len(data)} bytes')
+
+
+if __name__ == "__main__":
+    benchmark_feed_checksum()
