6aa1628e8850513fd479a8f82287408a0017aa35
guyskk <guyskk@qq.com>
2020-04-20 20:49:33 +0800

    fix rss cli refresh_feed performance

--- rssant_cli/rss.py
+++ rssant_cli/rss.py
@@ -381,20 +381,21 @@ def delete_feed(key):
 @click.option('--feeds', help="feed ids, separate by ','")
 @click.option('--union-feeds', help="union feed ids, separate by ','")
 @click.option('--key', help="feed url or title keyword")
-def refresh_feed(feeds, union_feeds, key):
-    feed_objs = []
+@click.option('--expire', type=int, default=1, help="expire hours")
+def refresh_feed(feeds, union_feeds, key, expire=None):
+    feed_ids = []
     if feeds:
-        feed_ids = _get_feed_ids(feeds)
-        feed_objs.extend([Feed.get_by_pk(x) for x in feed_ids])
+        feed_ids.extend(_get_feed_ids(feeds))
     if union_feeds:
-        feed_ids = _decode_union_feed_ids(union_feeds)
-        feed_objs.extend([Feed.get_by_pk(x) for x in feed_ids])
+        feed_ids.extend(_decode_union_feed_ids(union_feeds))
     if key:
         cond = Q(url__contains=key) | Q(title__contains=key)
-        feeds = list(Feed.objects.filter(cond).all())
-        feed_objs.extend(feeds)
-    expire_at = time.time() + 60 * 60
-    for feed in tqdm.tqdm(feed_objs, ncols=80, ascii=True):
+        feed_objs = Feed.objects.filter(cond).only('id').all()
+        feed_ids.extend(x.id for x in feed_objs)
+    feed_ids = list(sorted(set(feed_ids)))
+    expire_at = time.time() + expire * 60 * 60
+    for feed_id in tqdm.tqdm(feed_ids, ncols=80, ascii=True):
+        feed = Feed.objects.only('id', 'url', 'use_proxy').get(pk=feed_id)
         scheduler.tell('worker_rss.sync_feed', dict(
             feed_id=feed.id,
             url=feed.url,
