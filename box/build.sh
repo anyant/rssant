43dc0e9aa751ad561374fb20fa0193c5bda24da6
guyskk <guyskk@qq.com>
2020-05-04 18:18:00 +0800

    update build script, support run test in docker

--- box/build.sh
+++ box/build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-# git clone https://gitee.com/anyant/rssant-web.git box/web
-# pushd box/web; git pull; popd
+set -e
 
-docker build -f box/Dockerfile -t rssant/box:latest . $@
+current_repo=$(git remote get-url origin)
+web_repo=$(python -c '
+import sys;
+p=sys.argv[1].rsplit("/",1);
+w=p[1].replace("rssant","rssant-web");
+print(p[0]+"/"+w)
+' "$current_repo")
+echo "rssant-web: $web_repo"
+
+if [ ! -d "box/web" ]; then
+    git clone "$web_repo" box/web
+fi
+pushd box/web || exit 1
+git fetch
+git checkout master
+git pull
+popd || exit 1
+
+docker build -f box/Dockerfile -t guyskk/rssant:latest . $@
