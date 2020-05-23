88811c0b8e9c30b400d9fbfe6eb01cc6ea29bb2c
guyskk <guyskk@qq.com>
2020-05-23 23:32:18 +0800

    update feed_story_stat model

--- rssant_api/models/feed_story_stat.py
+++ rssant_api/models/feed_story_stat.py
@@ -18,3 +18,27 @@ class FeedStoryStat(Model):
 
     unique_ids_data = models.BinaryField(
         **optional, max_length=100 * 1024, help_text="unique ids data")
+
+    @classmethod
+    def _create_if_not_exists(cls, feed_id: int) -> bool:
+        is_exists = FeedStoryStat.objects.filter(pk=feed_id).exists()
+        if not is_exists:
+            FeedStoryStat(id=feed_id).save()
+
+    @classmethod
+    def _create_or_update(cls, feed_id, **kwargs):
+        cls._create_if_not_exists(feed_id)
+        FeedStoryStat.objects.filter(pk=feed_id)\
+            .update(**kwargs)
+
+    @classmethod
+    def save_unique_ids_data(cls, feed_id: int, unique_ids_data: bytes):
+        cls._create_or_update(feed_id, unique_ids_data=unique_ids_data)
+
+    @classmethod
+    def save_monthly_story_count_data(cls, feed_id: int, monthly_story_count_data: bytes):
+        cls._create_or_update(feed_id, monthly_story_count_data=monthly_story_count_data)
+
+    @classmethod
+    def save_checksum_data(cls, feed_id: int, checksum_data: bytes):
+        cls._create_or_update(feed_id, checksum_data=checksum_data)
