834a60ce6b9a3698a0b4408cac490e1bcfc38bf4
guyskk <guyskk@qq.com>
2020-08-01 13:41:08 +0800

    disable actor registery storage, avoid startup issues

--- rssant_scheduler/actors/registery.py
+++ rssant_scheduler/actors/registery.py
@@ -3,7 +3,6 @@ import logging
 from actorlib import actor, ActorContext, NodeSpecSchema
 from validr import T
 
-from rssant_api.models import Registery
 from rssant_common.helper import pretty_format_json
 from rssant_common.actor_helper import django_context
 
@@ -15,9 +14,6 @@ LOG = logging.getLogger(__name__)
 @django_context
 def do_save_registery(ctx: ActorContext):
     LOG.info('save registery info for {}'.format(ctx.registery.registery_node.name))
-    registery_node = ctx.registery.registery_node.to_spec()
-    nodes = ctx.registery.to_spec()
-    Registery.create_or_update(registery_node, nodes)
     ctx.tell('scheduler.boardcast_registery')
 
 
@@ -25,14 +21,8 @@ def do_save_registery(ctx: ActorContext):
 @django_context
 def do_load_registery(ctx: ActorContext):
     registery_node = ctx.registery.registery_node.name
-    LOG.info(f'load registery info for {registery_node}')
-    registery = Registery.get(registery_node)
-    if registery:
-        ctx.registery.update(registery.node_specs)
-        title = 'loaded'
-    else:
-        title = 'current'
-    LOG.info(f'{title} registery info:\n' + pretty_format_json(ctx.registery.to_spec()))
+    registery_info = pretty_format_json(ctx.registery.to_spec())
+    LOG.info(f'load registery info for {registery_node}:\n' + registery_info)
     ctx.tell('scheduler.boardcast_registery')
 
 
