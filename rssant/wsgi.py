044e84c33720bbb5b8feee2f6fc6f2fb1cafd1e8
guyskk <guyskk@qq.com>
2021-11-13 14:11:17 +0800

    refactor wsgi entrypoint

--- rssant/wsgi.py
+++ rssant/wsgi.py
@@ -7,14 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 import backdoor
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application as _get_app
 import rssant_common.django_setup  # noqa:F401
 from rssant_config import CONFIG
 from rssant_common.logger import configure_logging
 from rssant_common.helper import is_main_or_wsgi
 
 
-if is_main_or_wsgi(__name__):
+def get_wsgi_application():
     configure_logging(level=CONFIG.log_level)
     backdoor.setup()
+    return _get_app()
+
+
+if is_main_or_wsgi(__name__):
     application = get_wsgi_application()
