458ecc1e23321d2d4656a42ce2c0dacb4548836f
guyskk <guyskk@qq.com>
2022-01-03 23:00:42 +0800

    remove image proxy active api

--- rssant_async/views.py
+++ rssant_async/views.py
@@ -28,25 +28,7 @@ async def image_proxy_view_v2(
             expires=CONFIG.image_token_expires)
     except ImageTokenDecodeError as ex:
         return json_response({'message': str(ex)}, status=400)
-    owner = request.cookies.get(IMAGE_OWNER_COOKIE)
-    if (not image_token.owner) or image_token.owner != owner:
-        err_msg = '403 Forbidden Access / not owner'
-        return json_response({'message': err_msg}, status=403)
     response = await image_proxy(request, url, image_token.referrer)
-    return response
-
-
-@routes.get('/image/active-proxy')
-async def image_proxy_active(request, user_id: T.str.maxlen(32)):
-    response = json_response({'message': 'OK'})
-    response.set_cookie(
-        IMAGE_OWNER_COOKIE,
-        user_id,
-        path='/api/v1/image',
-        httponly=True,
-        samesite='None',
-        secure=True,
-    )
     return response
 
 
