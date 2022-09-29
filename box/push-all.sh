9267d74078b5c142b11f790ea8eca94bdfb56e10
guyskk <guyskk@qq.com>
2022-09-29 11:34:49 +0800

    fix push docker manifest

--- box/push-all.sh
+++ box/push-all.sh
@@ -12,6 +12,7 @@ if [ $VERSION != 'latest' ]; then
     docker push guyskk/rssant:$VERSION-arm64
     docker push guyskk/rssant:$VERSION-amd64
     echo "*** Push manifest guyskk/rssant:$VERSION ***"
+    docker manifest rm guyskk/rssant:$VERSION || true
     docker manifest create guyskk/rssant:$VERSION \
         --amend guyskk/rssant:$VERSION-arm64 \
         --amend guyskk/rssant:$VERSION-amd64
@@ -22,6 +23,7 @@ echo "*** Push guyskk/rssant:latest-* ***"
 docker push guyskk/rssant:latest-arm64
 docker push guyskk/rssant:latest-amd64
 echo "*** Push manifest guyskk/rssant:latest ***"
+docker manifest rm guyskk/rssant:latest || true
 docker manifest create guyskk/rssant:latest \
     --amend guyskk/rssant:latest-arm64 \
     --amend guyskk/rssant:latest-amd64
