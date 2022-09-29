c85eb12443154305ca4093f98f9736b1ac5a8651
guyskk <guyskk@qq.com>
2022-09-29 11:25:43 +0800

    fix push docker images script

--- box/build-all.sh
+++ box/build-all.sh
@@ -19,15 +19,3 @@ bash box/build.sh --platform linux/amd64 -t guyskk/rssant:latest-amd64
 if [ $VERSION != 'latest' ]; then
     docker tag guyskk/rssant:latest-amd64 guyskk/rssant:$VERSION-amd64
 fi
-
-echo "*** Create manifest guyskk/rssant:latest ***"
-docker manifest create guyskk/rssant:latest \
-    --amend guyskk/rssant:latest-arm64 \
-    --amend guyskk/rssant:latest-amd64
-
-if [ $VERSION != 'latest' ]; then
-    echo "*** Create manifest guyskk/rssant:$VERSION ***"
-    docker manifest create guyskk/rssant:$VERSION \
-        --amend guyskk/rssant:$VERSION-arm64 \
-        --amend guyskk/rssant:$VERSION-amd64
-fi
---
+++ box/push-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    VERSION='latest'
+fi
+
+if [ $VERSION != 'latest' ]; then
+    echo "*** Push guyskk/rssant:$VERSION-* ***"
+    docker push guyskk/rssant:$VERSION-arm64
+    docker push guyskk/rssant:$VERSION-amd64
+    echo "*** Push manifest guyskk/rssant:$VERSION ***"
+    docker manifest create guyskk/rssant:$VERSION \
+        --amend guyskk/rssant:$VERSION-arm64 \
+        --amend guyskk/rssant:$VERSION-amd64
+    docker manifest push guyskk/rssant:$VERSION
+fi
+
+echo "*** Push guyskk/rssant:latest-* ***"
+docker push guyskk/rssant:latest-arm64
+docker push guyskk/rssant:latest-amd64
+echo "*** Push manifest guyskk/rssant:latest ***"
+docker manifest create guyskk/rssant:latest \
+    --amend guyskk/rssant:latest-arm64 \
+    --amend guyskk/rssant:latest-amd64
+docker manifest push guyskk/rssant:latest
