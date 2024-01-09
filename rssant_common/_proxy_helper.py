e114f5ab66d3e3c79a298bd066861d10b7e265bf
guyskk <guyskk@qq.com>
2024-01-09 16:55:39 +0800

    fix get_proxy_options

--- rssant_common/_proxy_helper.py
+++ rssant_common/_proxy_helper.py
@@ -1,13 +1,14 @@
 import random
 
 from rssant_config import CONFIG
+from rssant_feedlib.processor import is_use_proxy_url
 
 from .ezproxy import EZPROXY_SERVICE
 
 
 def choice_proxy(*, proxy_url, rss_proxy_url) -> bool:
     if proxy_url and rss_proxy_url:
-        use_rss_proxy = random.random() > 0.5
+        use_rss_proxy = random.random() > 0.67
     else:
         use_rss_proxy = bool(rss_proxy_url)
     return use_rss_proxy
@@ -21,6 +22,9 @@ def get_proxy_options(url: str = None) -> dict:
         proxy_url = EZPROXY_SERVICE.pick_proxy(url=url)
         if proxy_url:
             options.update(proxy_url=proxy_url)
+            # 特殊处理的URL，不使用RSS代理
+            if url and is_use_proxy_url(url):
+                return options
     if CONFIG.rss_proxy_enable:
         options.update(
             rss_proxy_url=CONFIG.rss_proxy_url,
