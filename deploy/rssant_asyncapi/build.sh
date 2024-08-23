8519837c13ff3829ab34aefd602d4fd9c4173756
guyskk <guyskk@qq.com>
2024-08-23 16:22:59 +0800

    fix rssant_asyncapi rename

--- deploy/rssant_asyncapi/build.sh
+++ deploy/rssant_asyncapi/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # shellcheck disable=SC2068
 ezfaas build \
-    --repository registry.cn-zhangjiakou.aliyuncs.com/rssant/rssant-async \
+    --repository registry.cn-zhangjiakou.aliyuncs.com/rssant/rssant-asyncapi \
     --dockerfile deploy/rssant_asyncapi/Dockerfile \
     --build-platform linux/amd64 \
     $@
--- deploy/rssant_asyncapi/deploy.sh
+++ deploy/rssant_asyncapi/deploy.sh
@@ -4,8 +4,8 @@ set -e
 
 # shellcheck disable=SC2068
 ezfaas deploy-aliyun \
-    --repository registry.cn-zhangjiakou.aliyuncs.com/rssant/rssant-async \
+    --repository registry.cn-zhangjiakou.aliyuncs.com/rssant/rssant-asyncapi \
     --dockerfile deploy/rssant_asyncapi/Dockerfile \
-    --function rssant-async \
+    --function rssant-asyncapi-qa \
     --build-id \
     $@
