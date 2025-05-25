b3bf0e263a29d7af391b63959d176e4bd4b9e426
guyskk <guyskk@qq.com>
2025-05-25 15:38:15 +0800

    change deploy to global

--- deploy/rssant_asyncapi/deploy-prod.sh
+++ deploy/rssant_asyncapi/deploy-prod.sh
@@ -4,7 +4,7 @@ set -e
 
 # shellcheck disable=SC2068
 ezfaas deploy-aliyun \
-    --repository registry.cn-zhangjiakou.aliyuncs.com/rssant/rssant-asyncapi \
+    --repository registry.ap-northeast-1.aliyuncs.com/rssant/rssant-asyncapi \
     --dockerfile deploy/rssant_asyncapi/Dockerfile \
     --function rssant-asyncapi \
     --build-id \
--- deploy/rssant_server/build.sh
+++ deploy/rssant_server/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # shellcheck disable=SC2068
 ezfaas build \
-    --repository registry.cn-zhangjiakou.aliyuncs.com/rssant/rssant-server \
+    --repository registry.ap-northeast-1.aliyuncs.com/rssant/rssant-server \
     --dockerfile deploy/rssant_server/Dockerfile \
     --build-platform linux/amd64 \
     $@
